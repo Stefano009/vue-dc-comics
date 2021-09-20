e2853f82e335a16ccc164396a2c38f2843b7c349
Stefano <stefano.funaro@gmail.com>
2021-09-20 18:01:16 +0200

    footer-list-done

--- src/assets/data/Links.js
+++ src/assets/data/Links.js
@@ -1,4 +1,3 @@
-2
 const headerLinks = [{
         text: 'characters',
         url: '#',
@@ -51,4 +50,147 @@ const headerLinks = [{
     }
 ];
 
-export { headerLinks };
+const footerLinks = [{
+        title: 'DC COMICS',
+        data: [{
+                text: "characters",
+                url: "#",
+                current: false
+            },
+            {
+                text: "comics",
+                url: "#",
+                current: false
+            },
+            {
+                text: "movies",
+                url: "#",
+                current: false
+            },
+            {
+                text: "TV",
+                url: "#",
+                current: false
+            },
+            {
+                text: "games",
+                url: "#",
+                current: false
+            },
+            {
+                text: "videos",
+                url: "#",
+                current: false
+            },
+            {
+                text: "news",
+                url: "#",
+                current: false
+            },
+        ],
+    },
+    {
+        title: 'SHOP',
+        data: [{
+                text: "shop DC",
+                url: "#",
+                current: false
+            },
+            {
+                text: "shop DC collectibles",
+                url: "#",
+                current: false
+            },
+        ],
+    },
+    {
+        title: 'DC',
+        data: [{
+                text: "term of use",
+                url: "#",
+                current: false
+            },
+            {
+                text: "privacy policy(new)",
+                url: "#",
+                current: false
+            },
+            {
+                text: "ad choices",
+                url: "#",
+                current: false
+            },
+            {
+                text: "advertising",
+                url: "#",
+                current: false
+            },
+            {
+                text: "jobs",
+                url: "#",
+                current: false
+            },
+            {
+                text: "subscriptions",
+                url: "#",
+                current: false
+            },
+            {
+                text: "talent workshops",
+                url: "#",
+                current: false
+            },
+            {
+                text: "CPSC certificates",
+                url: "#",
+                current: false
+            },
+            {
+                text: "ratings",
+                url: "#",
+                current: false
+            },
+            {
+                text: "shope help",
+                url: "#",
+                current: false
+            },
+            {
+                text: "contact us",
+                url: "#",
+                current: false
+            },
+        ],
+    },
+    {
+        title: 'SITES',
+        data: [{
+                text: "DC",
+                url: "#",
+                current: false
+            },
+            {
+                text: "MAD magazine",
+                url: "#",
+                current: false
+            },
+            {
+                text: "DC kids",
+                url: "#",
+                current: false
+            },
+            {
+                text: "DC univers",
+                url: "#",
+                current: false
+            },
+            {
+                text: "DC power visa",
+                url: "#",
+                current: false
+            },
+        ],
+    },
+]
+
+export { headerLinks, footerLinks };
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,63 @@
 <template>
-  <div>
-    <h1>Footer</h1>
+  <div class="footer">
+    <ul class="outer-list">
+      <li :class="(link.current) ? 'active' : null" v-for="(link, index) in links" :key="index">
+        <h3>{{link.title}}</h3>
+        <ul>
+          <li v-for="(data, index ) in links[index].data" :key="index">
+            <a :href="link.url">
+              {{ data.text }}
+            </a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+    <img src="../assets/img/dc-logo-bg.png" alt="">
   </div>
 </template>
 
 <script>
+import * as Links from '@/assets/data/Links.js';
 export default {
   name: 'Footer',
+  data() {
+    return {
+      links: Links.footerLinks
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+@import "@/style/signatureColors";
+@import "@/style/mixins";
+.footer {
+  background-image: url('../assets/img/footer-bg.jpg');
+  @include flex('spacearound');
+  align-items: center;
+  width: 100%;
+  height: 560px;
+  margin: 0 auto;
+  img {
+      height: 600px;
+      object-fit: contain;
+      overflow: hidden;
+    }
+  .outer-list {
+    ul {
+      padding: 0;
+    }
+    @include flex('column');
+    height: 300px;
+    width: 30%;
+    list-style-type: none;
+    padding: 0;
+    li {
+      list-style-type: none;
+      text-align: start;
+      color: white;
+    }
+  }
+}
 </style>
